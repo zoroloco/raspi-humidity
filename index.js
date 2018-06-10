20c6fcd149be58fe64e99430c635ca32b164c993
System Administrator <root@Kenneths-MBP.lan>
2018-06-10 08:34:22 -0500

    hi

--- index.js
+++ index.js
@@ -3,29 +3,18 @@ var pathUtil = require('path'),
     log      = require(pathUtil.join(__dirname,'./logger.js')),
     conf     = require(pathUtil.join(__dirname,'./conf.json')),
     cp       = require('child_process'),
-    http     = require('http'),
-    buffer   = require('buffer').Buffer;
+    http     = require('http');
 
 log.init();
 var cmd     = pathUtil.join(__dirname,"AdafruitDHT.py");
+var client  = http.createClient(conf.remote_port, conf.remote_server);
+var request = client.request("POST", conf.remote_path, {"host":conf.remote_server});
 
 var findHumidity = function() {
 
     function post(msg) {
         log.info('Posting to: ' + conf.remote_server+conf.remote_path);
-        var body = JSON.stringify({data: msg});
-
-        var request = new http.ClientRequest({
-           hostname: conf.remote_server,
-            port: 8080,
-            path: conf.remote_path,
-            method: "POST",
-            headers: {
-               "Content-Type" : "application/json",
-                "Content-Length": Buffer.byteLength(body)
-            }
-        });
-        request.end(body);
+        request.write(msg);
     }
 
     var a2303 = cp.spawn('python', [cmd, '2302', conf.pin]);
